9aa862e596f2704068e9e8c15ca7e9bdd189d9ba
jboretsky <jonah.boretsky@gmail.com>
2017-05-29 20:47:56 -0700

    Add common Ngram feature
    train_ngrams will get ngrams that the article title and
    body have in common, and normalize that based on the
    length of the article body.

--- stance_detection.py
+++ stance_detection.py
@@ -9,11 +9,19 @@
 
 import pdb
 from csv import DictReader
+import nltk
+
+def get_ngrams(text, n):
+    tokens = nltk.word_tokenize(text)
+    tokens = [ token.lower() for token in tokens if len(token) > 1 ]
+    return nltk.ngrams(tokens, n)
 
 class StanceDetectionClassifier:
     def gen_training_features(self, bodies_fpath, stances_fpath):
         # load data and parse features. store in instance state
         self._read(bodies_fpath, stances_fpath)
+        unigrams = self._train_ngrams(1)
+        # bigrams = self._train_ngrams(2)
 
     def _read(self, bodies_fpath, stances_fpath):
         with open(bodies_fpath, 'r') as f:
@@ -32,6 +40,28 @@ class StanceDetectionClassifier:
                 body_id = int(line['Body ID'])
                 self._stances.append({'Headline': headline,
                     'Body ID': body_id, 'Stance': stance})
+
+    def _train_ngrams(self, n):
+        stance_similarities = []
+        body_bigrams = {}
+
+        for bodyId in self._bodies:
+            body_bigrams[bodyId] = get_ngrams(self._bodies[bodyId], n)
+
+        for stance in self._stances:
+            stance_bigrams = get_ngrams(stance['Headline'], n)
+            num_bigrams_common = 0
+            for bigram in stance_bigrams:
+                if bigram in body_bigrams[stance['Body ID']]:
+                    num_bigrams_common += 1
+            stance_similarities.append(num_bigrams_common)
+
+        # normalize the counts based on length of the article
+        for i in range(len(stance_similarities)):
+            body_id = self._stances[i]['Body ID']
+            stance_similarities[i] = float(stance_similarities[i])/len(self._bodies[body_id])
+
+        return stance_similarities
 
     def train(self):
         pass
