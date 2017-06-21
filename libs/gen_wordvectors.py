6919e18656f152944ce992812dfbfa86fe72d847
Programmingjoe <joeczepil@gmail.com>
2017-06-21 15:43:24 -0700

    Code cleanup, add reference to data

--- libs/gen_wordvectors.py
+++ libs/gen_wordvectors.py
@@ -4,39 +4,8 @@ from csv import DictReader
 
 class WordVector:
 
-    # def train(self, bodies_fpath, stances_fpath):
-    #     print 'Generating training features'
-    #     self._train_bodies, self._train_stances = self._read(bodies_fpath, stances_fpath, True)
-
-    # stances: [{'Headline': headline, 'Body ID': body_id, 'Stance': stance}, ..]
-    # def _read(self, bodies_fpath, stances_fpath, is_training):
-    #     with open(bodies_fpath, 'r') as f:
-    #         r = DictReader(f)
-    #         bodies_dict = {}
-    #         for line in r:
-    #             body = line['articleBody'].decode('utf-8')
-    #             bodies_dict[int(line['Body ID'])] = body
-    #
-    #     with open(stances_fpath, 'r') as f:
-    #         r = DictReader(f)
-    #         stances = []
-    #         for line in r:
-    #             headline = line['Headline'].decode('utf-8')
-    #             body_id = int(line['Body ID'])
-    #             if(is_training):
-    #                 stance = line['Stance'].decode('utf-8')
-    #                 stances.append({
-    #                         'Headline': headline,
-    #                         'Body ID': body_id,
-    #                         'Stance': stance})
-    #             else:
-    #                 stances.append({
-    #                         'Headline': headline,
-    #                         'Body ID': body_id})
-    #
-    #     return bodies_dict, stances
-
     def gen_wordvectors(self, dataset, body_ids, stances):
+        # https://cs.fit.edu/~mmahoney/compression/textdata.html
         sentences = word2vec.Text8Corpus('text8')
         model = word2vec.Word2Vec(sentences, size=200)
         wordvectordists = []
